4be3d1db2522cf14d5cdad0c7ef35ff4999abe1d
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-14 09:50:30 -0300

    feat: adding the back images to LoginScreen; use of Stack and Align widgets

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -6,55 +6,65 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32), // To wrap the column with padding 32
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,  // To vertically center the column
-          children: <Widget>[
-            Image.asset(
-              "assets/images/logo.png", 
-              width: 120,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch, // To stretch the components, button in this case, to maximum width
-              children: [
-                const SizedBox(height: 32),
-                const Text(
-                  "Sistema de Gestão de Contas",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 32,
-                  ),),
-                const SizedBox(height: 32),
-                TextFormField(
-                  decoration: InputDecoration(
-                    label: Text("E-mail"),  // placeholder
-                  ),
+      body: Stack(
+        children: [
+          Image.asset("assets/images/banner.png"),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset("assets/images/stars.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32), // To wrap the column with padding 32
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,  // To vertically center the column
+              children: <Widget>[
+                const SizedBox(height: 190),
+                Image.asset(
+                  "assets/images/logo.png", 
+                  width: 100,
                 ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    label: Text("Senha"),  // placeholder
-                  ),
-                ),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(
-                      Color(0xFFFFA902)
-                    ), // A value of FF before the color code means the color is 100% opaque (not transparent at all).
-                  ),
-                  child: const Text(
-                    "Entrar", 
-                    style: TextStyle(color: Colors.black,),
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch, // To stretch the components, button in this case, to maximum width
+                  children: [
+                    const SizedBox(height: 24),
+                    const Text(
+                      "Sistema de Gestão de Contas",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),),
+                    const SizedBox(height: 8),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("E-mail"),  // placeholder
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        label: Text("Senha"),  // placeholder
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFFFFA902)
+                        ), // A value of FF before the color code means the color is 100% opaque (not transparent at all).
+                      ),
+                      child: const Text(
+                        "Entrar", 
+                        style: TextStyle(color: Colors.black,),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
