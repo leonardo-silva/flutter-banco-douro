e5e10c18c684c1047681241e9c50d28f660d81f1
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-14 09:34:13 -0300

    feat: adding references to images in pubspec.yaml and using the logo in LoginScreen

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -10,39 +10,48 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(32), // To wrap the column with padding 32
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,  // To vertically center the column
-          crossAxisAlignment: CrossAxisAlignment.stretch, // To stretch the components, button in this case, to maximum width
           children: <Widget>[
-            const Text(
-              "Sistema de Gestão de Contas",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 32,
-              ),),
-            const SizedBox(height: 32),
-            TextFormField(
-              decoration: InputDecoration(
-                label: Text("E-mail"),  // placeholder
-              ),
+            Image.asset(
+              "assets/images/logo.png", 
+              width: 120,
             ),
-            const SizedBox(height: 16),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                label: Text("Senha"),  // placeholder
-              ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(
-                  Color(0xFFFFA902)
-                ), // A value of FF before the color code means the color is 100% opaque (not transparent at all).
-              ),
-              child: const Text(
-                "Entrar", 
-                style: TextStyle(color: Colors.black,),
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch, // To stretch the components, button in this case, to maximum width
+              children: [
+                const SizedBox(height: 32),
+                const Text(
+                  "Sistema de Gestão de Contas",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),),
+                const SizedBox(height: 32),
+                TextFormField(
+                  decoration: InputDecoration(
+                    label: Text("E-mail"),  // placeholder
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text("Senha"),  // placeholder
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      Color(0xFFFFA902)
+                    ), // A value of FF before the color code means the color is 100% opaque (not transparent at all).
+                  ),
+                  child: const Text(
+                    "Entrar", 
+                    style: TextStyle(color: Colors.black,),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
