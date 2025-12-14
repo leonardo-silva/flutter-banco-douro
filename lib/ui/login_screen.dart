1c0302b4467e78fd7dbbc4279838e9c309d9a3bd
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-14 09:03:00 -0300

    feat: setting style for the button and using SizedBox to put spaces (margins) between components

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -10,6 +10,7 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(32), // To wrap the column with padding 32
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,  // To vertically center the column
+          crossAxisAlignment: CrossAxisAlignment.stretch, // To stretch the components, button in this case, to maximum width
           children: <Widget>[
             const Text(
               "Sistema de Gestão de Contas",
@@ -17,20 +18,31 @@ class LoginScreen extends StatelessWidget {
               style: TextStyle(
                 fontSize: 32,
               ),),
+            const SizedBox(height: 32),
             TextFormField(
               decoration: InputDecoration(
                 label: Text("E-mail"),  // placeholder
               ),
             ),
+            const SizedBox(height: 16),
             TextFormField(
               obscureText: true,
               decoration: InputDecoration(
                 label: Text("Senha"),  // placeholder
               ),
             ),
+            const SizedBox(height: 32),
             ElevatedButton(
               onPressed: () {},
-              child: const Text("Entrar"),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  Color(0xFFFFA902)
+                ), // A value of FF before the color code means the color is 100% opaque (not transparent at all).
+              ),
+              child: const Text(
+                "Entrar", 
+                style: TextStyle(color: Colors.black,),
+              ),
             ),
           ],
         ),
